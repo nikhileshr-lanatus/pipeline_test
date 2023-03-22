97b8ad3067e6e0d0ab5285010b055196979254b5
nikhileshr-lanatus <nikhilesh.r@lanatussystems.com>
2023-03-22 15:23:30 +0530

    add comments

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 cd /home/ubuntu/server
 
 # npm install
-# npm install --save react react-dom react-scripts 
+# npm install --save react react-dom react-scripts
 #react-particles-js
 
 # npm install pm2 -g
@@ -13,10 +13,10 @@ cd /home/ubuntu/server
 
 # sudo rm -r /root/BigBeyond/big-beyond
 
-sudo rm -r -r /root/BigBeyond/big-beyond-aws/server
+# sudo rm -r -r /root/BigBeyond/big-beyond-aws/server
 
-sudo cp /home/ubuntu/server -r /root/BigBeyond/big-beyond-aws/server
+# sudo cp /home/ubuntu/server -r /root/BigBeyond/big-beyond-aws/server
 
-sudo rm -r /home/ubuntu/server
+# sudo rm -r /home/ubuntu/server
 
 
--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,39 +1,39 @@
-# cd /home/ubuntu/server
-# npm start
-# pm2 start npm --name "covidapp" -- start
-# pm2 startup 
+# # cd /home/ubuntu/server
+# # npm start
+# # pm2 start npm --name "covidapp" -- start
+# # pm2 startup
 
-sudo su -
+# sudo su -
 
-sudo pm2 stop all
+# sudo pm2 stop all
 
-cd /root/BigBeyond/big-beyond-aws/server
+# # cd /root/BigBeyond/big-beyond-aws/server
 
-sudo pm2 delete BigBeyondDemoFrontend
-sudo pm2 serve build 3000 --name BigBeyondDemoFrontend --spa
+# sudo pm2 delete BigBeyondDemoFrontend
+# sudo pm2 serve build 3000 --name BigBeyondDemoFrontend --spa
 
-# start backend
-cd /root/BigBeyond/big-beyond-aws/server/functions
+# # start backend
+# # cd /root/BigBeyond/big-beyond-aws/server/functions
 
-sudo npm install
+# # sudo npm install
 
-cp /root/BigBeyond/demoEnv/backend/.env /root/BigBeyond/big-beyond-aws/server/functions/.env
+# # cp /root/BigBeyond/demoEnv/backend/.env /root/BigBeyond/big-beyond-aws/server/functions/.env
+# #
+# # mv /root/BigBeyond/big-beyond-aws/server/functions/Prisma /root/BigBeyond/big-beyond-aws/server/functions/prisma
 
-mv /root/BigBeyond/big-beyond-aws/server/functions/Prisma /root/BigBeyond/big-beyond-aws/server/functions/prisma
+# # npx prisma generate
 
-npx prisma generate
+# # npx prisma db push
 
-npx prisma db push
+# # sudo pm2 delete BigBeyondDemoApi
+# # sudo pm2 start yarn --name BigBeyondDemoApi -- dev
 
-sudo pm2 delete BigBeyondDemoApi
-sudo pm2 start yarn --name BigBeyondDemoApi -- dev
-
-sudo pm2 save
+# # sudo pm2 save
 
 
-sudo pm2 restart all
+# sudo pm2 restart all
 
-# remove code deployment.
-# cd /opt/codedeploy-agent/deployment-root
+# # remove code deployment.
+# # cd /opt/codedeploy-agent/deployment-root
 
-# sudo rm -r -v !("deployment-instructions"|"deployment-logs"|"ongoing-deployment")
+# # sudo rm -r -v !("deployment-instructions"|"deployment-logs"|"ongoing-deployment")
