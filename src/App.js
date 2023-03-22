5cdee24a723e06f21e36b68934e96fae2279863e
nikhileshr-lanatus <nikhilesh.r@lanatussystems.com>
2023-03-22 12:26:51 +0530

    a

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        >a
           Learn React
         </a>
       </header>
